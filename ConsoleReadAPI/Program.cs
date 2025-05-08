8d5c33ec8d38d25a6a4fec7534a6d006a4674542
Richard Chalk <barmybrit@gmail.com>
2025-05-08 07:33:37 +0200

    Add project files.

---
+++ ConsoleReadAPI/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using SuperHeroAPIDemo_G_NET9.Models;
+using System.Net.Http.Headers;
+
+namespace ConsoleReadAPI
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Denna Console App anropar en web API som finns lokalt i samma solution!
+            Console.WriteLine("Anropar Web API...");
+            
+            // URL till vår lokal API endpoint
+            string apiUrl = "https://localhost:7096/api/SuperHero";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var superheroes = JsonConvert.DeserializeObject<List<SuperHero>>(responseData);
+
+                    Console.WriteLine("Svar från API:");
+                    Console.WriteLine("==============");
+                    foreach (var superhero in superheroes)
+                    {
+                        Console.WriteLine($"{superhero.Id}: {superhero.Name}");
+                    }
+
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Request error: {ex.Message}");
+                }
+            }
+
+            Console.WriteLine("Valfri knapp för att avsluta...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PSO/Program.cs
@@ -0,0 +1,51 @@
+
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPIDemo_G_NET9.Data;
+
+namespace SuperHeroAPIDemo_G_NET9
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers().AddNewtonsoftJson();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            // Lägg till min DbContext
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Lägg till min DataInitializer med Dependency Injection
+            builder.Services.AddTransient<DataInitializer>();
+
+            var app = builder.Build();
+
+            // Kör min MigrateData() metod
+            using (var scope = app.Services.CreateScope())
+            {
+                scope.ServiceProvider.GetService<DataInitializer>().MigrateData();
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                // Re-introduce Swagger UI
+                app.UseSwaggerUI(options =>
+                    options.SwaggerEndpoint("/openapi/v1.json", "superHero api"));
+
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
